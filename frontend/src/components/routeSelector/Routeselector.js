044496e99b50c50b12e0110f37d34a1565f780ae
Vishesh Goyal <68350319+VisheshGoyal25@users.noreply.github.com>
2022-05-01 23:15:17 +0530

    added voice command feature

--- frontend/src/components/routeSelector/Routeselector.js
+++ frontend/src/components/routeSelector/Routeselector.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import './Routeselector.css'
 import * as apiCall from './routeApifunc'
 import BusList from '../BusList/BusList'
+import alanBtn from '@alan-ai/alan-sdk-web';
 export default function Routeselector({setId:setId}) {
     const [dataInp, setData] = useState("")
     const [startCity, setStartCity] = useState('')
     const [destination, setDestination] = useState('')
+    
+    useEffect(() => {
+        alanBtn({
+            key: '076a72f0ba54f78358f8c74d5a2921752e956eca572e1d8b807a3e2338fdd0dc/stage',
+            onCommand: ({command,source,destination}) => {
+                console.log(source,destination)
+                setStartCity({ startCity: source });
+                setDestination({ destination: destination })
+                apiCall.getRoutesFromApi(source,destination)
+                .then(response => response.data)
+                .then(data => {
+                    console.log(data)
+                    setData(data.bus)
+                })
+                
+            }
+        });
+      }, []);
     const handleToCity = e => {
         e.preventDefault()
         setDestination({ destination: e.target.value })
@@ -19,8 +38,8 @@ export default function Routeselector({setId:setId}) {
         e.preventDefault()
         setStartCity({ startCity: e.target.value })
     }
-
-    const getRoutes = e => {
+   
+    const getRoutes = (e)=> {
         e.preventDefault()
         apiCall.getRoutesFromApi(startCity.startCity, destination.destination)
             .then(response => response.data)
